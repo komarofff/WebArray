dea32df56b02d6c9c1db0bd18d7854cd51eabc23
Anatoliy Komarov <admin@komaroff.biz>
2022-04-06 11:32:40 +0300

    new modals

--- webarray-test/src/js/modals.js
+++ webarray-test/src/js/modals.js
@@ -18,11 +18,6 @@ selects.forEach((el) => {
 
 
 
-//new file
-const newFile = new Modals({
-    selector: ".new-file"
-})
-newFile.click()
 
 
 // bookmarks
@@ -43,15 +38,4 @@ const editBookmark = new Modals({
 })
 editBookmark.clickAll()
 
-const editFile = new Modals({
-    selector: '.edit-file',
-    // data: JSON.stringify({
-    //     id: 1,
-    //     title: 'title',
-    //     link: 'https://webarray.ca',
-    //     notes: 'some notes here',
-    //     image: 'https://webarray.ca/images/test.jpg'
-    // })
-})
-editFile.click()
 
