9994d5f54851e9e75f634c6a5c6b4c75ffcb056a
Anatoliy Komarov <admin@komaroff.biz>
2022-04-06 10:59:59 +0300

    appen divs to dom

--- webarray-test/src/js/app.js
+++ webarray-test/src/js/app.js
@@ -9,7 +9,8 @@ document.addEventListener('mousemove', (e) => {
 function closeMainMenus() {
     const menus = document.querySelectorAll('.main-menu-box')
     menus.forEach((el) => {
-        el.classList.add('hidden')
+        //el.classList.add('hidden')
+        el.remove()
     })
 
 }
@@ -19,27 +20,234 @@ document.querySelector('.center-zone').addEventListener('click', (e) => {
     // e.preventDefault()
     closeMainMenus()
 })
+
 document.querySelector('.center-zone').addEventListener('contextmenu', (e) => {
     closeMainMenus()
     if (e.target.parentElement.classList.contains('folder')) {
         e.preventDefault()
         closeMainMenus()
-        document.querySelector('.folder-section').classList.remove('hidden')
-        if (e.target.dataset.data) {
-            document.querySelector('.folder-section').dataset.data = e.target.dataset.data
-        }
-        document.querySelector('.folder-section').style.cssText = `position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px`
-        mainMenu('folder-section')
+        // document.querySelector('.folder-section').classList.remove('hidden')
+        // if (e.target.dataset.data) {
+        //     document.querySelector('.folder-section').dataset.data = e.target.dataset.data
+        // }
+        // document.querySelector('.folder-section').style.cssText = `position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px`
+        let folderMenu = `<div class="folder-section main-menu-box  animation-popup " 
+         style="position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px"  data-data="${e.target.parentElement.dataset.data}">
+    <ul>
+        <li class="open-folder">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-arrow-up-right">
+                <line x1="7" y1="17" x2="17" y2="7"></line>
+                <polyline points="7 7 17 7 17 17"></polyline>
+            </svg>
+            <span>Open folder</span></li>
+        <li class="download">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-arrow-down-circle">
+                <circle cx="12" cy="12" r="10"></circle>
+                <polyline points="8 12 12 16 16 12"></polyline>
+                <line x1="12" y1="8" x2="12" y2="16"></line>
+            </svg>
+            <span>Zip & Download</span></li>
+    </ul>
+    <p class="stripe-gray"></p>
+    <ul>
+        <li class="flag">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-flag">
+                <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path>
+                <line x1="4" y1="22" x2="4" y2="15"></line>
+            </svg>
+            <span>Flag</span>
+            <ul class="flag-menu-box">
+                <li class="color-base">No flag</li>
+                <li class="color-important">Important</li>
+                <li class="color-approved">Approved</li>
+                <li class="color-final">Final</li>
+                <li class="color-good">Good</li>
+                <li class="color-old">Old</li>
+                <li class="color-bad">Bad</li>
+            </ul>
+        </li>
+    </ul>
+    <p class="stripe-gray"></p>
+    <ul>
+        <li class="share">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-link">
+                <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+            </svg>
+            <span>Share</span></li>
+    </ul>
+    <p class="stripe-gray"></p>
+    <ul>
+        <li class="rename">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-edit-3">
+                <path d="M12 20h9"></path>
+                <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+            </svg>
+            <span>Rename</span></li>
+        <li class="copy">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-copy">
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+            </svg>
+            <span>Copy</span></li>
+        <li class="delete">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-trash-2">
+                <polyline points="3 6 5 6 21 6"></polyline>
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                <line x1="10" y1="11" x2="10" y2="17"></line>
+                <line x1="14" y1="11" x2="14" y2="17"></line>
+            </svg>
+            <span>Delete</span></li>
+    </ul>
+
+</div>`
+        let newFolderFile = document.createElement('div')
+        newFolderFile.innerHTML = folderMenu
+        document.body.append(newFolderFile)
+        setTimeout(()=>{
+            mainMenu('folder-section')
+        },50)
+
+
     } else if (e.target.classList.contains('project')) {
         e.preventDefault()
         closeMainMenus()
-        //console.log(e.target.dataset.data)
-        document.querySelector('.project-section').classList.remove('hidden')
-        if (e.target.dataset.data) {
-            document.querySelector('.project-section').dataset.data = e.target.dataset.data
-        }
-        document.querySelector('.project-section').style.cssText = `position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px`
-        mainMenu('project-section')
+        let projectMenu = `<div class="project-section main-menu-box  animation-popup " 
+         style="position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px"  data-data="${e.target.dataset.data}">
+    <ul>
+        <li class="open-file">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-arrow-up-right">
+                <line x1="7" y1="17" x2="17" y2="7"></line>
+                <polyline points="7 7 17 7 17 17"></polyline>
+            </svg>
+            <span>Open</span></li>
+        <li class="preview">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-eye">
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+            </svg>
+            <span>Preview</span></li>
+        <li class="download">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-arrow-down-circle">
+                <circle cx="12" cy="12" r="10"></circle>
+                <polyline points="8 12 12 16 16 12"></polyline>
+                <line x1="12" y1="8" x2="12" y2="16"></line>
+            </svg>
+            <span>Download</span></li>
+    </ul>
+    <p class="stripe-gray"></p>
+    <ul>
+        <li class="flag">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-flag">
+                <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path>
+                <line x1="4" y1="22" x2="4" y2="15"></line>
+            </svg>
+            <span>Flag</span>
+            <ul class="flag-menu-box">
+                <li class="color-base">No flag</li>
+                <li class="color-important">Important</li>
+                <li class="color-approved">Approved</li>
+                <li class="color-final">Final</li>
+                <li class="color-good">Good</li>
+                <li class="color-old">Old</li>
+                <li class="color-bad">Bad</li>
+            </ul>
+
+        </li>
+        <li class="branch">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-layers">
+                <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                <polyline points="2 17 12 22 22 17"></polyline>
+                <polyline points="2 12 12 17 22 12"></polyline>
+            </svg>
+            <span>Branch</span></li>
+        <li class="edit-file">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-edit">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+            </svg>
+            <span>Edit</span>
+        </li>
+
+    </ul>
+    <p class="stripe-gray"></p>
+    <ul>
+        <li class="share">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-link">
+                <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+            </svg>
+            <span>Share</span></li>
+    </ul>
+    <p class="stripe-gray"></p>
+    <ul>
+        <li class="rename">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-edit-3">
+                <path d="M12 20h9"></path>
+                <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+            </svg>
+            <span>Rename</span></li>
+        <li class="copy">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-copy">
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+            </svg>
+            <span>Copy</span></li>
+        <li class="delete">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-trash-2">
+                <polyline points="3 6 5 6 21 6"></polyline>
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                <line x1="10" y1="11" x2="10" y2="17"></line>
+                <line x1="14" y1="11" x2="14" y2="17"></line>
+            </svg>
+            <span>Delete</span></li>
+    </ul>
+</div>`
+        let newMenuFile = document.createElement('div')
+        newMenuFile.innerHTML = projectMenu
+        document.body.append(newMenuFile)
+        setTimeout(()=>{
+            mainMenu('project-section')
+        },50)
+
     } else {
         e.preventDefault()
         closeMainMenus()
