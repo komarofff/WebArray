871900add1f7d4828ba4495fc66d114c7291525a
Anatoliy Komarov <admin@komaroff.biz>
2022-04-08 15:11:56 +0300

    +good-flag

--- webarray-test/src/js/file.js
+++ webarray-test/src/js/file.js
@@ -190,7 +190,7 @@ function showFileMenu(e) {
     </ul>
     <p class="stripe-gray"></p>
     <ul>
-        <li class="flag">
+        <li class="flag"  onclick="flagFile()">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                  stroke-linejoin="round" class="feather feather-flag">
@@ -502,7 +502,9 @@ function fileClick(event){
     document.querySelector('.file-information').classList.remove('hidden')
 
 }
-
+function flagFile(){
+    alert('flag file id='+ fileId)
+}
 function downloadFile() {
 
     let data = newData
