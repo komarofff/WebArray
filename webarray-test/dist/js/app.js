a3ee565e0d950af42d9289957242e6fe130995cd
Anatoliy Komarov <admin@komaroff.biz>
2022-04-08 15:36:56 +0300

    4

--- webarray-test/dist/js/app.js
+++ webarray-test/dist/js/app.js
@@ -209,34 +209,6 @@ const download = (path, filename) => {
     document.body.removeChild(anchor)
 }
 
-const value = document.getElementById('card')
-value.onmousedown = function (event) {
-
-    value.ondragstart = function () {
-        return false;
-    }
-    value.style.position = 'fixed'
-    //value.style.zIndex = 1000
-    document.body.append(value)
-    moveAt(event.pageX, event.pageY)
-
-    function moveAt(pageX, pageY) {
-        value.style.left = pageX - value.offsetWidth / 2 + 'px'
-        value.style.top = pageY - value.offsetHeight / 2 + 'px'
-    }
-
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY)
-    }
-
-    document.addEventListener('mousemove', onMouseMove)
-    value.onmouseup = function () {
-        document.removeEventListener('mousemove', onMouseMove)
-        value.onmouseup = null
-    }
-}
-
-
 
 
 
