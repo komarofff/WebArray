b28285988591cad72504d1b51ef9f3690353ee42
Anatoliy Komarov <admin@komaroff.biz>
2022-04-11 14:24:15 +0300

    +sort

--- webarray-test/dist/js/file.js
+++ webarray-test/dist/js/file.js
@@ -2,148 +2,7 @@ let newData = null
 let fileId = null
 let filesList = []
 let flags_lists = ['none', 'old', 'bad', 'good', 'important', 'approved', 'final']
-// function fileSection(e) {
-//     closeMainMenus()
-//     if (e.target.classList.contains('project')) {
-//         e.preventDefault()
-//         closeMainMenus()
-//         newData = JSON.parse(e.target.dataset.data)
-//         let projectMenu = `<div class="project-section main-menu-box  animation-popup "
-//          style="position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px" data-data='${e.target.dataset.data}'>
-//     <ul>
-//         <li class="open-file">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-arrow-up-right">
-//                 <line x1="7" y1="17" x2="17" y2="7"></line>
-//                 <polyline points="7 7 17 7 17 17"></polyline>
-//             </svg>
-//             <span>Open</span></li>
-//         <li class="preview">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-eye">
-//                 <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-//                 <circle cx="12" cy="12" r="3"></circle>
-//             </svg>
-//             <span>Preview</span></li>
-//         <li class="download">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none"
-//                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-//                  class="feather feather-arrow-down-circle">
-//                 <circle cx="12" cy="12" r="10"></circle>
-//                 <polyline points="8 12 12 16 16 12"></polyline>
-//                 <line x1="12" y1="8" x2="12" y2="16"></line>
-//             </svg>
-//             <span>Download</span></li>
-//     </ul>
-//     <p class="stripe-gray"></p>
-//     <ul>
-//         <li class="flag">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-flag">
-//                 <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path>
-//                 <line x1="4" y1="22" x2="4" y2="15"></line>
-//             </svg>
-//             <span>Flag</span>
-//             <ul class="flag-menu-box">
-//                 <li class="color-base">No flag</li>
-//                 <li class="color-important">Important</li>
-//                 <li class="color-approved">Approved</li>
-//                 <li class="color-final">Final</li>
-//                 <li class="color-good">Good</li>
-//                 <li class="color-old">Old</li>
-//                 <li class="color-bad">Bad</li>
-//             </ul>
-//
-//         </li>
-//         <li class="branch">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-layers">
-//                 <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-//                 <polyline points="2 17 12 22 22 17"></polyline>
-//                 <polyline points="2 12 12 17 22 12"></polyline>
-//             </svg>
-//             <span>Branch</span></li>
-//         <li class="edit-file" onclick="editFile();">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-edit">
-//                 <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-//                 <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-//             </svg>
-//             <span>Edit</span>
-//         </li>
-//
-//     </ul>
-//     <p class="stripe-gray"></p>
-//     <ul>
-//         <li class="share">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none"
-//                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-//                  class="feather feather-link">
-//                 <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
-//                 <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
-//             </svg>
-//             <span>Share</span></li>
-//     </ul>
-//     <p class="stripe-gray"></p>
-//     <ul>
-//         <li class="rename">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-edit-3">
-//                 <path d="M12 20h9"></path>
-//                 <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
-//             </svg>
-//             <span>Rename</span></li>
-//         <li class="copy">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-copy">
-//                 <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-//                 <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-//             </svg>
-//             <span>Copy</span></li>
-//         <li class="delete">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-trash-2">
-//                 <polyline points="3 6 5 6 21 6"></polyline>
-//                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-//                 <line x1="10" y1="11" x2="10" y2="17"></line>
-//                 <line x1="14" y1="11" x2="14" y2="17"></line>
-//             </svg>
-//             <span>Delete</span></li>
-//     </ul>
-// </div>`
-//         let newMenuFile = document.createElement('div')
-//         setTimeout(() => {
-//             newMenuFile.innerHTML = projectMenu
-//             document.body.appendChild(newMenuFile)
-//         }, 100)
-//
-//
-//         setTimeout(() => {
-//             mainMenu('project-section')
-//             //edit file
-//             // const editFile = new Modals({
-//             //     selector: '.edit-file',
-//             //     data: e.target.dataset.data
-//             // })
-//             // editFile.click()
-//             // editFile(JSON.parse(e.target.dataset.data))
-//             console.log(JSON.parse(e.target.dataset.data))
-//         }, 200)
-//
-//     }
-//
-//
-// }
+
 function showFiles() {
     // to get files data from server
     filesList = [
@@ -219,7 +78,9 @@ function showFiles() {
 
     ]
     //['none','old', 'bad', 'good', 'important', 'approved', 'final']
+    let counter = -1
     filesList.forEach((el) => {
+        counter++
         let data = JSON.stringify(el)
         let itemVar = 'item'
         if(el.flag==='bad'){
@@ -242,6 +103,7 @@ function showFiles() {
         newFile.setAttribute('data-flag', el.flag)
         newFile.setAttribute('data-data', data)
         newFile.setAttribute('data-startid', el.id)
+        newFile.setAttribute('data-arrid', counter)
         newFile.setAttribute('oncontextmenu', 'showFileMenu(event)')
         newFile.innerHTML = fileBox
         document.querySelector('.center-zone__inner-section').appendChild(newFile)
@@ -693,7 +555,7 @@ function downloadFile() {
 
     let data = newData
     url = newData.link
-    download('url', newData.fileName);
+    download('url', newData.l);
 }
 
 function branchFile() {
--- webarray-test/src/js/folder.js
+++ webarray-test/src/js/folder.js
@@ -1,214 +1,108 @@
-// function folderSection(e){
-//     closeMainMenus()
-//     e.stopImmediatePropagation()
-//     console.log(e.target)
-//     if (e.target.parentElement.classList.contains('folder')  ) {
-//         e.preventDefault()
-//         closeMainMenus()
-//
-//         let folderMenu = `<div class="folder-section main-menu-box  animation-popup "
-//          style="position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px"  data-data='${e.target.parentElement.dataset.data}'>
-//     <ul>
-//         <li class="open-folder">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-arrow-up-right">
-//                 <line x1="7" y1="17" x2="17" y2="7"></line>
-//                 <polyline points="7 7 17 7 17 17"></polyline>
-//             </svg>
-//             <span>Open folder</span></li>
-//         <li class="download">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none"
-//                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-//                  class="feather feather-arrow-down-circle">
-//                 <circle cx="12" cy="12" r="10"></circle>
-//                 <polyline points="8 12 12 16 16 12"></polyline>
-//                 <line x1="12" y1="8" x2="12" y2="16"></line>
-//             </svg>
-//             <span>Zip & Download</span></li>
-//     </ul>
-//     <p class="stripe-gray"></p>
-//     <ul>
-//         <li class="flag">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-flag">
-//                 <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path>
-//                 <line x1="4" y1="22" x2="4" y2="15"></line>
-//             </svg>
-//             <span>Flag</span>
-//             <ul class="flag-menu-box">
-//                 <li class="color-base">No flag</li>
-//                 <li class="color-important">Important</li>
-//                 <li class="color-approved">Approved</li>
-//                 <li class="color-final">Final</li>
-//                 <li class="color-good">Good</li>
-//                 <li class="color-old">Old</li>
-//                 <li class="color-bad">Bad</li>
-//             </ul>
-//         </li>
-//     </ul>
-//     <p class="stripe-gray"></p>
-//     <ul>
-//         <li class="share">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none"
-//                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-//                  class="feather feather-link">
-//                 <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
-//                 <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
-//             </svg>
-//             <span>Share</span></li>
-//     </ul>
-//     <p class="stripe-gray"></p>
-//     <ul>
-//         <li class="rename">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-edit-3">
-//                 <path d="M12 20h9"></path>
-//                 <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
-//             </svg>
-//             <span>Rename</span></li>
-//         <li class="copy">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-copy">
-//                 <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-//                 <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-//             </svg>
-//             <span>Copy</span></li>
-//         <li class="delete">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-//                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-//                  stroke-linejoin="round" class="feather feather-trash-2">
-//                 <polyline points="3 6 5 6 21 6"></polyline>
-//                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-//                 <line x1="10" y1="11" x2="10" y2="17"></line>
-//                 <line x1="14" y1="11" x2="14" y2="17"></line>
-//             </svg>
-//             <span>Delete</span></li>
-//     </ul>
-//
-// </div>`
-//         let newFolderFile = document.createElement('div')
-//         setTimeout(()=>{
-//             newFolderFile.innerHTML = folderMenu
-//             document.body.append(newFolderFile)
-//         },100)
-//
-//         setTimeout(() => {
-//             mainMenu('folder-section')
-//             console.log(JSON.parse(e.target.parentElement.dataset.data))
-//         }, 200)
-//
-//
-//     }
-// //     if (e.target.parentElement.parentElement.classList.contains('folder')  ) {
-// //         e.preventDefault()
-// //         closeMainMenus()
-// //
-// //         let folderMenu = `<div class="folder-section main-menu-box  animation-popup "
-// //          style="position:fixed; left: ${xCoordinate}px; top: ${yCoordinate}px"  data-data='${e.target.parentElement.parentElement.dataset.data}'>
-// //     <ul>
-// //         <li class="open-folder">
-// //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-// //                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-// //                  stroke-linejoin="round" class="feather feather-arrow-up-right">
-// //                 <line x1="7" y1="17" x2="17" y2="7"></line>
-// //                 <polyline points="7 7 17 7 17 17"></polyline>
-// //             </svg>
-// //             <span>Open folder</span></li>
-// //         <li class="download">
-// //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-// //                  fill="none"
-// //                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-// //                  class="feather feather-arrow-down-circle">
-// //                 <circle cx="12" cy="12" r="10"></circle>
-// //                 <polyline points="8 12 12 16 16 12"></polyline>
-// //                 <line x1="12" y1="8" x2="12" y2="16"></line>
-// //             </svg>
-// //             <span>Zip & Download</span></li>
-// //     </ul>
-// //     <p class="stripe-gray"></p>
-// //     <ul>
-// //         <li class="flag">
-// //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-// //                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-// //                  stroke-linejoin="round" class="feather feather-flag">
-// //                 <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path>
-// //                 <line x1="4" y1="22" x2="4" y2="15"></line>
-// //             </svg>
-// //             <span>Flag</span>
-// //             <ul class="flag-menu-box">
-// //                 <li class="color-base">No flag</li>
-// //                 <li class="color-important">Important</li>
-// //                 <li class="color-approved">Approved</li>
-// //                 <li class="color-final">Final</li>
-// //                 <li class="color-good">Good</li>
-// //                 <li class="color-old">Old</li>
-// //                 <li class="color-bad">Bad</li>
-// //             </ul>
-// //         </li>
-// //     </ul>
-// //     <p class="stripe-gray"></p>
-// //     <ul>
-// //         <li class="share">
-// //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-// //                  fill="none"
-// //                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-// //                  class="feather feather-link">
-// //                 <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
-// //                 <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
-// //             </svg>
-// //             <span>Share</span></li>
-// //     </ul>
-// //     <p class="stripe-gray"></p>
-// //     <ul>
-// //         <li class="rename">
-// //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-// //                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-// //                  stroke-linejoin="round" class="feather feather-edit-3">
-// //                 <path d="M12 20h9"></path>
-// //                 <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
-// //             </svg>
-// //             <span>Rename</span></li>
-// //         <li class="copy">
-// //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-// //                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-// //                  stroke-linejoin="round" class="feather feather-copy">
-// //                 <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-// //                 <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-// //             </svg>
-// //             <span>Copy</span></li>
-// //         <li class="delete">
-// //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-// //                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-// //                  stroke-linejoin="round" class="feather feather-trash-2">
-// //                 <polyline points="3 6 5 6 21 6"></polyline>
-// //                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-// //                 <line x1="10" y1="11" x2="10" y2="17"></line>
-// //                 <line x1="14" y1="11" x2="14" y2="17"></line>
-// //             </svg>
-// //             <span>Delete</span></li>
-// //     </ul>
-// //
-// // </div>`
-// //         let newFolderFile = document.createElement('div')
-// //         setTimeout(()=>{
-// //             newFolderFile.innerHTML = folderMenu
-// //             document.body.append(newFolderFile)
-// //         },100)
-// //
-// //         setTimeout(() => {
-// //             mainMenu('folder-section')
-// //             console.log(JSON.parse(e.target.parentElement.parentElement.dataset.data))
-// //         }, 200)
-// //
-// //
-// //     }
-// }
+let folderList = [
+    {
+        id: 0,
+        foldername: 'Folder 0',
+        link: 'https://webarray.ca/fggg.txt',
+        author: 'Test author 0',
+        createdAt: '22.03.2022',
+        image: '../images/test-image-bookmark-preview.jpg',
+        flag: 'none',
+        type: 'file'
+    },
+    {
+        id: 1,
+        foldername: 'Folder 1',
+        link: 'https://webarray.ca/fggg.txt',
+        author: 'Test author 1',
+        createdAt: '22.03.2021',
+        image: '../images/test-image-bookmark-preview.jpg',
+        flag: 'good',
+        type: 'file'
+    },
+    {
+        id: 2,
+        foldername: 'Folder 2',
+        link: 'https://webarray.ca/fggg.txt',
+        author: 'Test author 2',
+        createdAt: '22.04.2022',
+        image: '../images/test-image-bookmark-preview.jpg',
+        flag: 'important',
+        type: 'file'
+    },
+    {
+        id: 3,
+        foldername: 'Folder 3',
+        link: 'https://webarray.ca/fggg.txt',
+        author: 'Test author 3',
+        createdAt: '15.04.2022',
+        image: '../images/test-image-bookmark-preview.jpg',
+        flag: 'approved',
+        type: 'file'
+    }, {
+        id: 4,
+        foldername: 'Folder 4',
+        link: 'https://webarray.ca/fggg.txt',
+        author: 'Test author 4',
+        createdAt: '15.04.2022',
+        image: '../images/test-image-bookmark-preview.jpg',
+        flag: 'old',
+        type: 'file'
+    },
+    {
+        id: 5,
+        foldername: 'Folder 5',
+        link: 'https://webarray.ca/fggg.txt',
+        author: 'Test author 5',
+        createdAt: '15.04.2022',
+        image: '../images/test-image-bookmark-preview.jpg',
+        flag: 'final',
+        type: 'file'
+    },
+    {
+        id: 6,
+        foldername: 'Folder 6',
+        link: 'https://webarray.ca/fggg.txt',
+        author: 'Test author 6',
+        createdAt: '15.04.2022',
+        image: '../images/no-image.png',
+        flag: 'bad',
+        type: 'file'
+    },
+
+]
+folderList.sort((start,finish)=> finish.id - start.id)
+
+function showFolders() {
+    // to get files data from server
+
+    //['none','old', 'bad', 'good', 'important', 'approved', 'final']
+    let counter = -1
+
+    folderList.forEach((el) => {
+        counter++
+        let data = JSON.stringify(el)
+        let itemVar = 'item'
+        if(el.flag==='bad'){
+            itemVar = null
+        }
+        let fileBox = `
+                <div class="folder-name">
+                    <p>${el.foldername}</p>
+                </div>
+            `
+        let newFolder= document.createElement('div')
+        newFolder.setAttribute('class', 'relative folder')
+        newFolder.setAttribute('data-data', data)
+        newFolder.setAttribute('data-startid', el.id)
+        newFolder.setAttribute('data-arrid', counter)
+        newFolder.setAttribute('oncontextmenu', 'folderMenu(event)')
+        newFolder.innerHTML = fileBox
+        document.querySelector('.center-zone__inner-section').prepend(newFolder)
+    })
+
+}
+showFolders()
+
 let id= null
 function folderMenu(e) {
     e.preventDefault()
