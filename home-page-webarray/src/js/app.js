3409b69a9a745612e8058466279676c8c665b95a
Anatoliy Komarov <admin@komaroff.biz>
2022-04-14 15:22:51 +0300

    2

--- home-page-webarray/src/js/app.js
+++ home-page-webarray/src/js/app.js
@@ -1,7 +1,20 @@
-// const topMenu = document.querySelector('.top-menu')
-// topMenu.addEventListener('mouseover',(e)=>{
-//     if(e.target.classList.contains('open-drop')){
-//         e.target.parentNode.querySelector('.subcategory-2').style.display = 'flex'
-//     }
-//
-// })
+let topMenu = document.querySelector('.top-navbar')
+let smallNav = document.querySelector('.small-nav')
+document.addEventListener('scroll', () => {
+    let yCoordinate = window.pageYOffset
+    if (yCoordinate > 0) {
+        topMenu.classList.add('bg-white')
+        smallNav.classList.add('d-none')
+    } else {
+        topMenu.classList.remove('bg-white')
+        smallNav.classList.remove('d-none')
+    }
+
+    if (scrollY > yCoordinate) {
+        smallNav.classList.remove('d-none')
+    }
+    if (scrollY < yCoordinate) {
+        smallNav.classList.add('d-none')
+    }
+    scrollY = yCoordinate
+})
