6d3e86caa58651d43c6cfc495a27988891959f4f
Anatoliy Komarov <admin@komaroff.biz>
2022-04-14 12:30:12 +0300

    4

--- tailwind/dist/js/app.js
+++
@@ -1,7 +0,0 @@
-// const topMenu = document.querySelector('.top-menu')
-// topMenu.addEventListener('mouseover',(e)=>{
-//     if(e.target.classList.contains('open-drop')){
-//         e.target.parentNode.querySelector('.subcategory-2').style.display = 'flex'
-//     }
-//
-// })
